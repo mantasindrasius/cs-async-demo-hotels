58e0e35cda7387b9dea7b2d72f91ef38d96cfd7c
Mantas Indrašius <mantas@indrasius.lt>
2014-11-25 20:50:51 +0200

    Latest changes

---
+++ Lib.Test/WeatherAPITest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Diagnostics;
+
+namespace Lib.Test
+{
+    [TestClass]
+    public class WeatherAPITest
+    {
+        [TestMethod]
+        public void GetWeatherTest()
+        {
+            var api = new WeatherAPI();
+            //var hotel = new Hotel("hotel-1", "Hotel 1", "Ct8l4ZzadI7YwqtHvun8xB482Nl81FzsDeOGThl2Wtc.eyJpbnN0YW5jZUlkIjoiMTM4ZTAwZWEtMzI4NC04ODY5LWViYmMtMDMxNGQ4ODU0NTQ5Iiwic2lnbkRhdGUiOiIyMDE0LTExLTI1VDA2OjM4OjAxLjI1MloiLCJ1aWQiOiJmZGUwMTUxMi04ZWVkLTRmNDItODc4Zi1iODkxYTdhMWJlNjYiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiaXBBbmRQb3J0IjoiODguMTE5LjE1MC4xOTYvMzQ4MDEiLCJ2ZW5kb3JQcm9kdWN0SWQiOm51bGwsImRlbW9Nb2RlIjpmYWxzZX0");
+
+            var forecast = api.GetForecast();
+
+            Assert.IsTrue(forecast.Count > 0);
+        }
+    }
+}
---
+++ Lib/Utils.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public static class Utils
+    {
+        private static JsonSerializerSettings jsonSettings = new JsonSerializerSettings()
+        {
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            Error = ErrorHandler
+        };
+
+        public static HttpClient MakeRestClient(string url)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+
+        public static A ReadJson<A>(string source)
+        {
+            return JsonConvert.DeserializeObject<A>(source, jsonSettings);
+            //var reader = new JsonReader();
+            //return serializer.Deserialize<A>(source);
+        }
+
+        public static void ErrorHandler(object x, ErrorEventArgs error)
+        {
+            Console.WriteLine(error.ErrorContext.Error);
+            error.ErrorContext.Handled = true;
+        }
+
+        public static A UnwrapResult<A>(Task<A> task)
+        {
+            task.Wait();
+
+            return task.Result;
+        }
+    }
+}
